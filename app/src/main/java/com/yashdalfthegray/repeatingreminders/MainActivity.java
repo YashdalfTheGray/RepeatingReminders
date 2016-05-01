80185d9b73c32c34af834ea4895cc2e02b2fab67
YashdalfTheGray <yash.kulshrestha@gmail.com>
2016-04-30 20:00:30 -0400

    wired up recycler view in the main activity

--- app/src/main/java/com/yashdalfthegray/repeatingreminders/MainActivity.java
+++ app/src/main/java/com/yashdalfthegray/repeatingreminders/MainActivity.java
@@ -4,18 +4,26 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.yashdalfthegray.repeatingreminders.adapters.RecyclerAdapter;
+import com.yashdalfthegray.repeatingreminders.models.Reminder;
+
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerAdapter adapter;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -27,6 +35,19 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        setupRecyclerView();
+    }
+
+    private void setupRecyclerView() {
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.reminders_recycler);
+        adapter = new RecyclerAdapter(this, Reminder.getData());
+        assert recyclerView != null;
+        recyclerView.setAdapter(adapter);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(linearLayoutManager);
     }
 
     @Override
