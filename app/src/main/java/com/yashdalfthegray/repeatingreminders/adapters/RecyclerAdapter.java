03ae079f4e519e14ce5ae2a3187eb086ce46d72f
YashdalfTheGray <yash.kulshrestha@gmail.com>
2016-05-02 20:20:24 -0400

    added functionality to support the new reminder item

--- app/src/main/java/com/yashdalfthegray/repeatingreminders/adapters/RecyclerAdapter.java
+++ app/src/main/java/com/yashdalfthegray/repeatingreminders/adapters/RecyclerAdapter.java
@@ -1,17 +1,22 @@
 package com.yashdalfthegray.repeatingreminders.adapters;
 
 import android.content.Context;
+import android.content.res.Resources;
+import android.support.design.widget.Snackbar;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextClock;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.yashdalfthegray.repeatingreminders.R;
 import com.yashdalfthegray.repeatingreminders.models.Reminder;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 /**
  * Created by Yash Kulshrestha on 4/21/2016.
@@ -58,27 +63,33 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.Remind
 
     class ReminderViewHolder extends RecyclerView.ViewHolder {
 
-        TextView title;
-        TextClock start, end, interval;
+        TextView title, reminderText;
+        ImageButton editReminderButton;
         int position;
         Reminder current;
+        Resources res;
 
         public ReminderViewHolder(View itemView) {
             super(itemView);
+            res = itemView.getResources();
 
             title = (TextView) itemView.findViewById(R.id.reminder_title);
-            start = (TextClock) itemView.findViewById(R.id.reminder_start);
-            end = (TextClock) itemView.findViewById(R.id.reminder_end);
-            interval = (TextClock) itemView.findViewById(R.id.reminder_interval);
+            reminderText = (TextView) itemView.findViewById(R.id.reminder_text);
+            editReminderButton = (ImageButton) itemView.findViewById(R.id.button_edit_reminder);
         }
 
         public void setData(Reminder currentObj, final int position) {
             this.current = currentObj;
             this.position = position;
             this.title.setText(current.getTitle());
-            this.start.setText(current.getStart().toString());
-            this.end.setText(current.getEnd().toString());
-            this.interval.setText(current.getInterval().toString());
+            SimpleDateFormat sdf = new SimpleDateFormat("h:mm a", Locale.US);
+            this.reminderText.setText(String.format(res.getString(R.string.reminder_text), sdf.format(new Date()), sdf.format(new Date())));
+            this.editReminderButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Snackbar.make(v.getRootView().findViewById(R.id.main_app_view), "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show();
+                }
+            });
         }
     }
 }
